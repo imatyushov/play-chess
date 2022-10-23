8c9942875ee46fa53e74de9d3f2102d50f7ad692
Ilya <80617082+imatyushov@users.noreply.github.com>
2022-10-23 13:08:26 +0300

    first commit

---
+++ src/models/figures/Figure.ts
@@ -0,0 +1,43 @@
+import logo from '../../assets/black-king.png'
+import {Colors} from "../Colors";
+import {Cell} from "../Cell";
+
+export enum FigureNames {
+    FIGURE = 'Фигура',
+    KING = 'Король',
+    KNIGHT = 'Конь',
+    PAWN = 'Пешка',
+    QUEEN = 'Ферзь',
+    ROOK = 'Ладья',
+    BISHOP = 'Слон'
+}
+
+export class Figure {
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureNames;
+    id: number;
+
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.logo = null;
+        this.name = FigureNames.FIGURE
+        this.id = Math.random()
+    }
+
+    public canMove(target: Cell) : boolean {
+        if(target.figure?.color === this.color)
+            return false
+        if(target.figure?.name === FigureNames.KING)
+            return false
+        return true;
+    }
+
+    public moveFigure(target: Cell) {
+
+    }
+}
